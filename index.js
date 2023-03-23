84f13d9a520bd24db2a52a105cf9791433db5683
Scott Taylor <rscott.taylor01@gmail.com>
2023-03-23 12:18:07 -0500

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(names, events) {
+    const cards = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${events} gift!`;
+        cards.push(message)
+    }
+    return cards;
+}
+
+
+function countDown(num) {
+    let i = num
+    while (i >= 0) {
+        console.log(i--)
+    }
+    
+}
